31379557867c5e468f7fa8ba7ce8e11b2c597277
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-07-04 22:04:33 +0900

    Set base path

--- demos/wasm/vite.config.js
+++ demos/wasm/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/ghostpdl/",
   plugins: [react()],
   // https://stackoverflow.com/a/79466574
   assetsInclude: ["**/*.wasm"],
